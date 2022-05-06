c78f6f885cdeb495aa29f04343cb97a20ae09867
Saumya <saumyaoec@gmail.com>
2022-05-06 22:28:32 +0530

    create_view,list_view

--- BookingApp/urls.py
+++ BookingApp/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path, include
 from . import views
+# from .views import detail_view
+
 
 app_name = 'booking'
 urlpatterns = [
-    path('create/', views.CreateBooking.as_view(), name='create_booking'),
-]
+    path("create/",views.create_view),
+    path("list_view/",views.list_view, name ='list_view'),
+    
+
+    ]
+
+
+    
--- BookingApp/views.py
+++ BookingApp/views.py
@@ -1,17 +1,50 @@
-from django.shortcuts import render, redirect
-from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .forms import BookingForm
+from .models import Booking,Address
+# from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
-from BookingApp.forms import BookingForm
-
-# Create your views here.
 
 
-class CreateBooking(CreateView):
-    #model = Property
-    form_class = BookingForm
-    template_name = 'booking/create_booking.html'
 
-    def form_valid(self, form):
-        data = form.save(commit=False)
-        data.save()
-        return redirect('/')
+
+def create_view(request):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+ 
+    # add the dictionary during initialization
+    form = BookingForm(request.POST )
+    if form.is_valid():
+        form.save()
+    context['form']= form
+    return render(request, "create_booking.html",context)
+
+
+def list_view(request):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+    # add the dictionary during initialization
+    context['dataset'] = Booking.objects.all()
+
+    return render(request, "list_view.html", context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
