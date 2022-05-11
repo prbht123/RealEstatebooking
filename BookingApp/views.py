4dd96b845d0f7ba3af89d5bf59f5025ea6bb59ac
prabhat <prabhat.webkrone@gmail.com>
2022-05-11 12:46:38 +0530

    Updated booking module functionality

--- BookingApp/views.py
+++ BookingApp/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
-
+from .models import Booking
 from BookingApp.forms import BookingForm
 
 # Create your views here.
 
 
-class CreateBooking(CreateView):
-    #model = Property
+class createBooking(CreateView):
+    """
+        Functionality for booking to the property.
+    """
     form_class = BookingForm
     template_name = 'booking/create_booking.html'
 
@@ -15,8 +17,20 @@ class CreateBooking(CreateView):
         data = form.save(commit=False)
         data.paid = False
         data.save()
-        print(data)
-        print(data)
-        print(data.property)
-        print(data.property.slug)
         return redirect('payment:process', slug=data.property.slug)
+
+
+class bookingUpdateView(UpdateView):
+    """
+        This class is used for updating of a particular property.
+    """
+    model = Booking
+    form_class = BookingForm
+    template_name = 'booking/update_booking.html'
+    success_url = '/'
+
+    def get_form_kwargs(self):
+        kwargs = super(bookingUpdateView, self).get_form_kwargs()
+        print(kwargs['instance'])
+        kwargs.update()
+        return kwargs
