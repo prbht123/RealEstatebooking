a0ad27bec812519568cb9968882e03e1e2158059
prabhat <prabhat.webkrone@gmail.com>
2022-05-18 12:03:38 +0530

    created popular locations functionality

--- AdminUser/models.py
+++ AdminUser/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Permission
+from django_countries.fields import CountryField
 # Create your models here.
 
 
@@ -19,3 +20,19 @@ class AdminUserRoles(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.user.username)
         super(AdminUserRoles, self).save(*args, **kwargs)
+
+
+class PopularLocations(models.Model):
+    id = models.AutoField(primary_key=True)
+    slug = models.SlugField(max_length=250, null=True)
+    state = models.CharField(max_length=100, null=True)
+    city = models.CharField(max_length=100, null=True)
+    country = CountryField(null=True)
+    popular_rank = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.city
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.city)
+        super(PopularLocations, self).save(*args, **kwargs)
