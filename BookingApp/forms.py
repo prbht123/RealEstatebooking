6897c6ce2e7168aa3b029bc152a492f963b776fd
prabhat <prabhat.webkrone@gmail.com>
2022-05-19 12:19:02 +0530

    Implemented design of booking page

--- BookingApp/forms.py
+++ BookingApp/forms.py
@@ -6,4 +6,4 @@ class BookingForm(ModelForm):
     class Meta:
         model = Booking
         fields = ['gender', 'firstname', 'lastname', 'nationality', 'address', 'email', 'mobile_Number',
-                  'property', 'date_from', 'date_until', 'creation_date', 'booking_status', 'cost', 'notes']
+                  'property', 'date_from', 'date_until', 'creation_date', 'booking_status', 'notes', 'cost']
