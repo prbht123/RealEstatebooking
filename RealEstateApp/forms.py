fe5938e8f2327498144fe19828796fba185848c0
prabhat <prabhat.webkrone@gmail.com>
2022-05-19 12:48:49 +0530

    Implemented design for create and update property page

--- RealEstateApp/forms.py
+++ RealEstateApp/forms.py
@@ -9,7 +9,7 @@ class PropertyForm(forms.ModelForm):
     class Meta:
         model = Property
         fields = ['property_name', 'room_type', 'bed_room', 'beds', 'bed_type', 'guest', 'Address',
-                  'cost', 'property_status', 'contact_number', 'cancellation', 'Description']
+                  'cost', 'property_status', 'contact_number', 'Description', 'cancellation']
 
 
 class FeedbackForm(forms.ModelForm):
