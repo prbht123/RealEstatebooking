208bc9d080f1b196afb0d8038cf3873f367e49ae
Smarak <smarak.webkrone@gmail.com>
2022-05-31 10:11:44 +0530

    Initial Commit

--- RealEstateProject/settings.py
+++ RealEstateProject/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import environ
-from decouple import config
+# import environ
+# from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
